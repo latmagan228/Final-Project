51061d4addb8995fe97f588fca0526dbcab4ddc0
latmagan228 <latmagan228@gmail.com>
2022-06-21 15:07:49 +0200

    h

--- src/machine.c
+++ src/machine.c
@@ -14,6 +14,9 @@ int counter;
 word_t stack[STACK_SIZE];
 int ssize;
 int sp;
+FILE *fp;
+FILE *out;
+FILE *in;
 };
 
 struct gvariables gv;
@@ -26,40 +29,37 @@ static uint32_t swap_uint32(uint32_t num)
 int init_ijvm(char *binary_file)
 {
   char *binary = binary_file;
-  FILE *fp;
-  fp = fopen(binary, "rb");
+  gv.fp = fopen(binary, "rb");
 
   uint32_t magicn = 0; 
-  fread(&magicn, sizeof(uint32_t), 1, fp);
+  fread(&magicn, sizeof(uint32_t), 1, gv.fp);
   magicn = swap_uint32(magicn);
 
   uint32_t cporigin;
-  fread(&cporigin, sizeof(uint32_t), 1, fp);
+  fread(&cporigin, sizeof(uint32_t), 1, gv.fp);
   cporigin = swap_uint32(cporigin);
 
   uint32_t cpsize;
-  fread(&cpsize, sizeof(uint32_t), 1, fp);
+  fread(&cpsize, sizeof(uint32_t), 1, gv.fp);
   cpsize = swap_uint32(cpsize);
 
   uint32_t* cpdata = (uint32_t*) malloc(cpsize/4*sizeof(uint32_t));
   for(int i = 0; i < cpsize/4; i++) 
   {
-    fread(&cpdata[i], sizeof(uint32_t), 1, fp);
+    fread(&cpdata[i], sizeof(uint32_t), 1, gv.fp);
     cpdata[i] = swap_uint32(cpdata[i]);
   } 
 
   uint32_t torigin;
-  fread(&torigin, sizeof(uint32_t), 1, fp);
+  fread(&torigin, sizeof(uint32_t), 1, gv.fp);
   torigin = swap_uint32(torigin);
 
-  fread(&gv.tsize, sizeof(uint32_t), 1, fp);
+  fread(&gv.tsize, sizeof(uint32_t), 1, gv.fp);
   gv.tsize = swap_uint32(gv.tsize);
 
   uint8_t* tdata = (uint8_t*) malloc(gv.tsize*sizeof(uint8_t));
-  fread(tdata, sizeof(char), gv.tsize, fp);
-
+  fread(tdata, sizeof(char), gv.tsize, gv.fp);
   gv.text = tdata;
-  fclose(fp);
 
   return 1;
 }
@@ -70,6 +70,7 @@ void destroy_ijvm()
   gv.sp = 0;
   gv.tsize = 0;
   gv.counter = 0;
+  fclose(gv.fp);
 }
 
 void run()
@@ -79,12 +80,14 @@ void run()
 
 void set_input(FILE *fp)
 {
-  // TODO: implement me
+  gv.in = fp;
+  gv.in = stdin;
 } 
 
 void set_output(FILE *fp)
 {
-  // TODO: implement me
+  gv.out = fp;
+  gv.out = stdout;
 }
 
 int text_size(void){
@@ -103,9 +106,16 @@ word_t pop() {
   return gv.stack[gv.sp--];
 }
 
+int byte_to_int(){
+  byte_t *bytes = &gv.text[gv.counter + 1];
+  int result = (bytes[0] & 0xFF << 8) + (bytes[1] & 0xFF);
+  return result;
+}
+
 bool step(void){
   byte_t a;
   byte_t b;
+  int c;
   switch (gv.text[gv.counter]) {
     case OP_BIPUSH:
       printf("BIPUSH\n");
@@ -154,11 +164,16 @@ bool step(void){
       printf("ISWAP\n");
       printf("%x\n", (int8_t)gv.stack[gv.sp]);
       break;
+    case OP_GOTO:
+      printf(("GOTO\n"));
+      c = byte_to_int();
+      printf("%d\n", gv.counter);
+      printf("%d\n", c);
+      gv.counter += c - 1;
+      printf("%d\n",  gv.counter);
+      break;
     case OP_OUT:
       printf("OUT\n");
-      break;
-    case OP_NOP:
-      printf("NOP\n");
       break;
     case OP_LDC_W:
       gv.counter += 2;
@@ -181,12 +196,12 @@ bool step(void){
 
 byte_t *get_text(void){
 
-return gv.text;
+  return gv.text;
 }
 
 int get_program_counter(void){
 
-return gv.counter;
+  return gv.counter;
 }
 
 byte_t get_instruction(void){
@@ -208,11 +223,3 @@ int stack_size(void){
 
   return gv.sp;
 }
-
-
-
-
-
-
-
-
